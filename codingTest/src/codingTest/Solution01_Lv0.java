7242bed50498f1e6983fe3b3ff7f65b64543eac0
DESKTOP-8IA1NB6\DEV-775 <winwin3512@gmail.com>
2022-11-01 11:20:52 +0900

    프로그래머스 코딩입문테스트 JAVA

---
+++ codingTest/src/codingTest/Solution01_Lv0.java
@@ -0,0 +1,25 @@
+package codingTest;
+
+public class Solution01_Lv0 {
+
+	public static void main(String[] args) {
+		// 두 수의 나눗셈
+		
+		// 정수 num1과 num2가 매개변수로 주어질 때, 
+		// num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
+		
+		int num1 = 3;
+		int num2 = 2;
+		
+		int multiply = 1000;
+		
+		int answer = 0;
+		
+		//answer = (int)((double)num1 / num2 * multiply);
+		answer = multiply * num1 / num2;
+		
+		System.out.println(answer);
+
+	}
+
+}
